97031291609e04b54e92b69408fb5a316fc0c030
alina.fedyniak <a.fedynyak@artjoker.team>
2023-11-05 01:06:32 +0200

    block footer adaptive mobile, fix form block addToUs

--- js/main.js
+++ js/main.js
@@ -62,3 +62,23 @@ $(function() {
 $(function() {
     $("#go-top").scrollToTop();
 });
+
+// -------btn-to-top-2------
+$(function() {
+    $.fn.scrollToTop = function() {
+        $(this).hide().removeAttr("href");
+        if ($(window).scrollTop() >= "250") $(this).fadeIn("slow")
+        const scrollDiv = $(this);
+        $(window).scroll(function() {
+            if ($(window).scrollTop() <= "250") $(scrollDiv).fadeOut("slow")
+            else $(scrollDiv).fadeIn("slow")
+        });
+        $(this).click(function() {
+            $("html, body").animate({scrollTop: 0}, "slow")
+        })
+    }
+});
+
+$(function() {
+    $("#go-top2").scrollToTop();
+});
